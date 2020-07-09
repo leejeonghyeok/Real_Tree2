7ca6abf2914915e53bc135ed332364931aac14ad
Brian <brian11hwangb@gmail.com>
2020-07-09 11:54:57 +0900

    delete navigation

--- app/src/main/java/com/example/Real_Tree/Activity/MainActivity.java
+++ app/src/main/java/com/example/Real_Tree/Activity/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.Real_Tree.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,13 +11,20 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.Real_Tree.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bnv;
+    NavController  nc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bnv = (BottomNavigationView)findViewById(R.id.bottomNavigationView);
+        nc= Navigation.findNavController(this,R.id.fragment);
+        NavigationUI.setupWithNavController(bnv,nc);
+
+
 
 
     }
