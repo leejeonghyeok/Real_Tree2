e60866c3fd56c671775a9f30fa243d8e657f7465
Brian <brian11hwangb@gmail.com>
2020-07-14 11:48:35 +0900

    navigation button works

--- app/src/main/java/com/example/Real_Tree/Activity/MainActivity.java
+++ app/src/main/java/com/example/Real_Tree/Activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.Real_Tree.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
@@ -14,15 +16,24 @@ import com.example.Real_Tree.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    BottomNavigationView bnv;
-    NavController  nc;
+    BottomNavigationView btnnavigationview;
+    NavController  navcontroller;
+    DrawerLayout drawerlayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bnv = (BottomNavigationView)findViewById(R.id.bottomNavigation);
-        nc= Navigation.findNavController(this,R.id.fragment);
-        NavigationUI.setupWithNavController(bnv,nc);
+        btnnavigationview = (BottomNavigationView)findViewById(R.id.bottomNavigation);
+        navcontroller= Navigation.findNavController(this,R.id.fragment);
+        NavigationUI.setupWithNavController(btnnavigationview,navcontroller);
+        final DrawerLayout drawerlayout = (DrawerLayout)findViewById(R.id.drawer);
+
+        findViewById(R.id.menu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerlayout.openDrawer(GravityCompat.START);
+            }
+        });
 
 
 
