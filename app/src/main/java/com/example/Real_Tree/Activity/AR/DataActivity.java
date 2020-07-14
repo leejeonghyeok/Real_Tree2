53709b9e99888684f426d12b622af6a2dfeffb61
CSY <cotjdud1@g.skku.edu>
2020-07-14 13:43:28 +0900

    DataActivity2

--- app/src/main/java/com/example/Real_Tree/Activity/AR/DataActivity.java
+++ app/src/main/java/com/example/Real_Tree/Activity/AR/DataActivity.java
@@ -2,15 +2,30 @@ package com.example.Real_Tree.Activity.AR;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.example.Real_Tree.R;
 
 public class DataActivity extends AppCompatActivity {
 
+    EditText editName, editDBH, editLocation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
+
+        editName = (EditText)findViewById(R.id.editName);
+        editDBH = (EditText)findViewById(R.id.editDBH);
+        editLocation = (EditText)findViewById(R.id.editLocation);
+
+        Intent ARIntent = getIntent();
+        String name = ARIntent.getStringExtra("name");
+        editName.setText(name);
+        String dbh = ARIntent.getStringExtra("dbh");
+        editName.setText(dbh);
+        String location = ARIntent.getStringExtra("location");
+        editName.setText(location);
     }
 }
