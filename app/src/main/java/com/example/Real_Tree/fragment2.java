351c8af9a15fefd21efaf2509c18f4a31fe18108
leejeonghyeok <love011908@naver.com>
2020-07-14 14:57:39 +0900

    fra

--- app/src/main/java/com/example/Real_Tree/fragment2.java
+++ app/src/main/java/com/example/Real_Tree/fragment2.java
@@ -1,12 +1,15 @@
 package com.example.Real_Tree;
 
+
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +22,13 @@ public class fragment2 extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    Object m;
+    //SupportMapFragment mapFragment;
+    GoogleMap map;
+    private Marker currentMarker = null;
+    Marker trmark= null;
+    Marker selectedMarker=null;
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -26,6 +36,7 @@ public class fragment2 extends Fragment {
 
     public fragment2() {
         // Required empty public constructor
+
     }
 
     /**
@@ -49,6 +60,7 @@ public class fragment2 extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -58,7 +70,10 @@ public class fragment2 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment2, container, false);
+        View v=inflater.inflate(R.layout.fragment_fragment2, container, false);
+
+        return v;
     }
+
+
 }
