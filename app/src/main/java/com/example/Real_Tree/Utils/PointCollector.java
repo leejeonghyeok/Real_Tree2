60b15446a7a9aecf2f4fb1e8299d61246224d5a5
CSY <cotjdud1@g.skku.edu>
2020-07-20 14:28:52 +0900

    filterPoints Upgraded

--- app/src/main/java/com/example/Real_Tree/Utils/PointCollector.java
+++ app/src/main/java/com/example/Real_Tree/Utils/PointCollector.java
@@ -9,6 +9,7 @@ import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Map;
 
@@ -43,18 +44,18 @@ public class PointCollector {
         }
     }
 
-    public FloatBuffer filterPoints(){
+    public FloatBuffer filterPoints() {
 
         int numPoints = 0;
-        for(Map.Entry<Integer, LinkedList<float[]>> entry : allPoints.entrySet()){
-            if(entry.getValue().size() > 3){
+        for (Map.Entry<Integer, LinkedList<float[]>> entry : allPoints.entrySet()) {
+            if (entry.getValue().size() > 3) {
                 numPoints++;
             }
         }
         filterPoints = ByteBuffer.allocateDirect(numPoints * 4 * 4).order(ByteOrder.nativeOrder()).asFloatBuffer();
 
         for (Map.Entry<Integer, LinkedList<float[]>> entry : allPoints.entrySet()) {
-            if(entry.getValue().size() > 3){
+            if (entry.getValue().size() > 3) {
                 float mean_x = 0.0f, mean_y = 0.0f, mean_z = 0.0f;
                 for (float[] p : entry.getValue()) {
                     mean_x += p[0]; // x
@@ -65,66 +66,63 @@ public class PointCollector {
                 mean_x /= entry.getValue().size();
                 mean_y /= entry.getValue().size();
 
-                filterPoints.put(mean_x);
-                filterPoints.put(mean_y);
-                filterPoints.put(mean_z);
-                filterPoints.put(0);
-            }
-            /*
-            if (list.size() < 5) {
-                filterPoints.put(mean_x);
-                filterPoints.put(mean_y);
-                filterPoints.put(mean_z);
-                filterPoints.put(0);
-                continue;   // no more calculation
-            }
-            float distance_mean = 0.f;
-            float variance = 0.f;
-            for (float[] tmp : list) {
-                float sqDistance = (float)(Math.pow((tmp[0] - mean_x), 2.0) + Math.pow((tmp[1] - mean_y), 2.0) + Math.pow((tmp[2] - mean_z), 2.0));
-                variance += sqDistance;
-                distance_mean += Math.sqrt(sqDistance);
-            }
-            distance_mean /= list.size();
-            variance = (variance / list.size()) - distance_mean*distance_mean;
+                if (entry.getValue().size() < 5) {
+                    filterPoints.put(mean_x);
+                    filterPoints.put(mean_y);
+                    filterPoints.put(mean_z);
+                    filterPoints.put(0);
+                    continue; // no more calculation
+                }
 
-            // variance가 0일 때
-            if(variance == 0){
-                float[] finalPoint = new float[]{mean_x, mean_y, mean_z};
-                filterPoints.put(id, finalPoint);
-                continue; // no more calculation
-            } else {
-                Iterator<float[]> iter = list.iterator();
-                while(iter.hasNext()){
-                    float[] tmp = iter.next();
-                    // pow: 제곱 // abs: 절댓값
-                    float sqDistance = (float)(Math.pow((tmp[0] - mean_x), 2) + Math.pow((tmp[1] - mean_y), 2) + Math.pow((tmp[2] - mean_z), 2));
-                    float z_score = (float)(Math.abs(Math.sqrt(sqDistance) - distance_mean) / Math.sqrt(variance));
-                    if (z_score >= 1.2f) {
-                        iter.remove();
+                // calculate variance
+                float distance_mean = 0.f;
+                float variance = 0.f;
+                for (float[] tmp : entry.getValue()) {
+                    float sqDistance = (float) (Math.pow((tmp[0] - mean_x), 2.0) + Math.pow((tmp[1] - mean_y), 2.0) + Math.pow((tmp[2] - mean_z), 2.0));
+                    variance += sqDistance;
+                    distance_mean += Math.sqrt(sqDistance);
+                }
+                distance_mean /= entry.getValue().size();
+                variance = (variance / entry.getValue().size()) - distance_mean * distance_mean;
+
+                // variance가 0일 때
+                if (variance == 0) {
+                    filterPoints.put(mean_x);
+                    filterPoints.put(mean_y);
+                    filterPoints.put(mean_z);
+                    filterPoints.put(0);
+                    continue; // no more calculation
+                } else {
+                    Iterator<float[]> iter = entry.getValue().iterator();
+                    while (iter.hasNext()) {
+                        float[] tmp = iter.next();
+                        float sqDistance = (float) (Math.pow((tmp[0] - mean_x), 2) + Math.pow((tmp[1] - mean_y), 2) + Math.pow((tmp[2] - mean_z), 2));
+                        float z_score = (float) (Math.abs(Math.sqrt(sqDistance) - distance_mean) / Math.sqrt(variance));
+                        if (z_score >= 1.2f) {
+                            iter.remove();
+                        }
                     }
+
+                    if (entry.getValue().size() == 0) continue;
+
+                    mean_x = 0.f;
+                    mean_y = 0.f;
+                    mean_z = 0.f;
+                    for (float[] tmp : entry.getValue()) {
+                        mean_x += tmp[0];
+                        mean_y += tmp[1];
+                        mean_z += tmp[2];
+                    }
+                    mean_z /= entry.getValue().size();
+                    mean_x /= entry.getValue().size();
+                    mean_y /= entry.getValue().size();
+
+                    filterPoints.put(mean_x);
+                    filterPoints.put(mean_y);
+                    filterPoints.put(mean_z);
+                    filterPoints.put(0);
                 }
-
-                if(list.size() == 0) continue;
-
-                mean_x = 0.f;
-                mean_y = 0.f;
-                mean_z = 0.f;
-                for (float[] tmp : list) {
-                    mean_x += tmp[0];
-                    mean_y += tmp[1];
-                    mean_z += tmp[2];
-                }
-                mean_z /= list.size();
-                mean_x /= list.size();
-                mean_y /= list.size();
-
-                filterPoints.put(mean_x);
-                filterPoints.put(mean_y);
-                filterPoints.put(mean_z);
-                filterPoints.put(0);
             }
-            */
         }
         filterPoints.position(0);
         return filterPoints;
