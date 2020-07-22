04473f53a3ed072c8605ab484c59e4684928a9e4
Brian <brian11hwangb@gmail.com>
2020-07-22 12:03:47 +0900

    splash screen added

---
+++ app/src/main/java/edu/skku/treearium/Activity/SplashActivity.java
@@ -0,0 +1,25 @@
+package edu.skku.treearium.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import edu.skku.treearium.R;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3*1000); // wait for 5 seconds
+    }
+}
